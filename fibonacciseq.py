0441208c4ba46ade1792dacf7189fa64d6829927
Will Long <completelyoffthegrid13@gmail.com>
2021-12-06 12:07:20 +0000

    Update fibonacciseq.py

--- fibonacciseq.py
+++ fibonacciseq.py
@@ -8,10 +8,12 @@ while total < 4000000:
     if C % 2 == 0:
         total += C  
     print(f"Next Number Extra in Sequence: {C}")
+
     A = B + C
     if A % 2 == 0:
         total += A
     print(f"Next Number Extra in Sequence: {A}")
+    
     B = A + C
     if B % 2 == 0:
         total += B
